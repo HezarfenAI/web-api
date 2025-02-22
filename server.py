30a3a418c51c80747c21aa6adb965277e580cd20
Ömer Asaf Karasu <omerasafkarasu06@gmail.com>
2025-02-22 23:13:19 +0300

    Add server.py with fake news checker endpoint

---
+++ server.py
@@ -0,0 +1,24 @@
+import os
+import sys
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+
+from fastapi import FastAPI
+from hezarfen_ai.model import ModelLoader
+
+app = FastAPI()
+
+model_loader = ModelLoader()
+
+@app.post("/v1/hezarfen/fake-news-checker")
+async def read_root(text: str):
+    try:
+        result = model_loader.ask(text)
+
+        return { "is_success": True, "analyze_result": "Gerçek" if result[0] == "TRUE" else "Sahte" }
+
+    except Exception as e:
+        return {
+            "error": str(e),
+            "is_success": False
+        }
