d98ecc9eaa9dcf8bcb8f7d7e8ec1dff3839036ed
Ömer Asaf Karasu <omerasafkarasu06@gmail.com>
2025-03-06 20:58:55 +0300

    Add model training and data addition endpoints

--- server.py
+++ server.py
@@ -1,5 +1,9 @@
 import os
 import sys
+import pandas as pd
+import csv
+
+from pydantic import BaseModel
 
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 
@@ -17,7 +21,13 @@ app.add_middleware(
     allow_headers=["*"],  # Tüm header'lara izin verir
 )
 
-model_loader = ModelLoader()
+model_loader = ModelLoader(csv_file="./turkish_news.csv", model_file="./hezarfen.pkl", dataset_file="./turkish_news.csv")
+model_loader.run_model()
+
+class AddDataModel(BaseModel):
+    text: str
+    label: str
+    url: str
 
 @app.post("/v1/hezarfen/fake-news-checker")
 async def read_root(text: str):
@@ -31,3 +41,25 @@ async def read_root(text: str):
             "error": str(e),
             "is_success": False
         }
+
+@app.post("/v1/hezarfen/teach")
+async def add_data(request: AddDataModel):
+
+    if os.path.exists('./datasets/generated_dataset.csv') is not True:
+        with open('./datasets/generated_dataset.csv', 'w') as file:
+            fieldnames = ['text', 'label', 'url']
+            writer = csv.DictWriter(file, fieldnames=fieldnames)
+            writer.writeheader()
+
+    df = pd.read_csv('./datasets/generated_dataset.csv')
+
+    with open('./datasets/generated_dataset.csv', 'a', newline='') as csvfile:
+        fieldnames = ['text', 'label', 'url']
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+        writer.writerow({
+            "text": request.text,
+            "label": request.label,
+            "url": request.url
+        })
+
+    return { "is_success": True }
